b7d6e27f6fa79c91cc6c355af8acf19f177a32e5
hejian65644955 <m15895420701@163.com>
2017-03-09 12:35:24 +0800

    Okhttp联网请求

--- app/src/main/java/android/hejian/com/myhomework/fragment/FirstFragment.java
+++ app/src/main/java/android/hejian/com/myhomework/fragment/FirstFragment.java
@@ -1,7 +1,17 @@
 package android.hejian.com.myhomework.fragment;
 
 import android.hejian.com.myhomework.R;
+import android.hejian.com.myhomework.utils.Constants;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * Created by 何健 on 2017/3/9.
@@ -9,14 +19,46 @@ import android.view.View;
 
 public class FirstFragment extends BaseFragment {
 
+    @InjectView(R.id.rv_first)
+    RecyclerView rvFirst;
+
     @Override
     public View initView() {
-        View view =View.inflate(mContext, R.layout.fragment_frist,null);
+        View view = View.inflate(mContext, R.layout.fragment_frist, null);
+        ButterKnife.inject(this, view);
         return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        OkHttpUtils.get()
+                .url(Constants.HOME_URL)
+                .id(100)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "联网请求失败"+e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "联网请求成功"+response);
+                       // processData(response);
+
+                    }
+                });
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
