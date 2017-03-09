1b9ca8c5526372943a7db1f3d60f239898836b05
hejian65644955 <m15895420701@163.com>
2017-03-09 10:03:45 +0800

    欢迎界面

--- app/src/main/java/android/hejian/com/myhomework/MainActivity.java
+++ app/src/main/java/android/hejian/com/myhomework/MainActivity.java
@@ -1,9 +1,11 @@
 package android.hejian.com.myhomework;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
